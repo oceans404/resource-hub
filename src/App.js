1b65ac3bd13c9c6f1c61012dec852a16594b4792
Steph <so.orpilla@gmail.com>
2022-05-20 14:48:35 -0400

    Set up Figment (DataHub) RPC and wallet connection mechanisms (Coinbase Wallet, WalletConnect, and Metamask)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { WalletLinkConnector } from '@web3-react/walletlink-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
+import { useWeb3React } from '@web3-react/core';
 import './App.css';
 
+const rpcNetwork = 'matic-mumbai--rpc';
+const rpcUrl = `https://${rpcNetwork}.datahub.figment.io/apikey/${process.env.REACT_APP_FIGMENT_KEY}`;
+
+const CoinbaseWallet = new WalletLinkConnector({
+  url: rpcUrl,
+  appName: 'Web3-react Demo',
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+  rpcUrl: rpcUrl,
+  bridge: 'https://bridge.walletconnect.org',
+  qrcode: true,
+});
+
+const Injected = new InjectedConnector({
+  supportedChainIds: [1, 3, 4, 5, 42],
+});
+
 function App() {
+  const { activate, deactivate, active, chainId, account } = useWeb3React();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button
+        onClick={() => {
+          activate(CoinbaseWallet);
+        }}
+      >
+        Coinbase Wallet
+      </button>
+      <button
+        onClick={() => {
+          activate(WalletConnect);
+        }}
+      >
+        Wallet Connect
+      </button>
+      <button
+        onClick={() => {
+          activate(Injected);
+        }}
+      >
+        Metamask
+      </button>
+
+      <button onClick={deactivate}>Disconnect</button>
+
+      <div>Connection Status: ${active}</div>
+      <div>Account: ${account}</div>
+      <div>Network ID: ${chainId}</div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function getLibrary(provider) {
+  return new Web3Provider(provider);
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+  <Web3ReactProvider getLibrary={getLibrary}>
     <App />
-  </React.StrictMode>
+  </Web3ReactProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
